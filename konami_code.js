ad4d465a59ba9d8810f55a0487b9d0d4b7a54e8b
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-08 07:06:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,31 +12,26 @@ const codes = [
 ];
 
 function init() {
-  document.body.addEventListener ('keydown', function(e) {
-
-
-function onKeyDownHandler(e) {
+  
+  var index = 0;
+  
+  var body = document.querySelector('body');
+  
+  body.addEventListener('keydown', function onKeyDownHandler(e){
+    
   var key = e.key;
-
-      // check if key is equal to current value of index in code array
-      // then loop through the array if conditions match
-
-    if (key === codes[index]) {
-        // if yes, increment index by 1
-        index++;
-
-        // if still in the loop, check if index is equal to code array length
-      if (index === codes.length - 1) {
-          // reset the index counter
-          index = 0;
-          // if yes, you passed, congrats!
-        alert("KONAMI CODE!");
-      }
-
-    // if key doesn't match current index in code array, then reset the index to zero
-    } else {
-      index = 0;
-    }
+  
+  if(key === code[0])
+  index++;
+  
+  if(index === code.length){
+    alert("Here's 30 Lives!");
+    
+  }
+  
+  }else{
+    index = 0;
+  }
   });
-}
+  
 }
