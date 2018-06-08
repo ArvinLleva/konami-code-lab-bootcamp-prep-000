47d7cf27b3683ba55a04aa04cb846d5a47d4eebe
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-08 07:20:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,17 +17,17 @@ function init() {
   
   var body = document.querySelector('body');
   
-  // name of the event, and a function to handle the event
+  //the listener event takes to arguments, the name of the event, and a function to handle the event
   body.addEventListener('keydown', function onKeyDownHandler(e){
   
   var key = e.key;
   
   //this checks if the key that is pressed is equal to the current key value of the index
   //this iterates though the array if the conditions match
-  
   if(key === codes[index]){
   index++; // this increases by one, or goes to the next index if they match
   
+  //if the pressed key continues till the last index of the array then you get an alert which congratualats you
   if(index === codes.length){
     alert("Here's 30 Lives!");
     index = 0;
